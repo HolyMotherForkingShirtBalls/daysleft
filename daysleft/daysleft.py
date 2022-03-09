b12655bb81d064983322fd88514916b087c863d9
Alex West <alexreidwest@gmail.com>
2022-03-09 09:12:19 -0600

    Implemented new countdown method

--- daysleft/daysleft.py
+++ daysleft/daysleft.py
@@ -35,14 +35,14 @@ countSec()
 def countMin():
     global ctM
     ctM = int(time.strftime("%M", t))
-    if ctM == 0:
+    if ctM == 0 and 0 <= ctS < 2:
         countHr()
 countMin()
 
 def countHr():
     global ctH
     ctH = int(time.strftime("%H", t))
-    if ctH == 0:
+    if ctH == 0 and ctS < 2:
         getDate()
 countHr()
 
@@ -53,93 +53,119 @@ def init():
     countMin()
     countHr()
 
-def countdown():
-    pass
+def countRemSec():
+    global cRS
+    countSec()
+    cRS = 59 - ctS
+    if cRS == 0:
+        countRemMin()
+countRemSec()
+
+def countRemMin():
+    global cRM
+    countMin()
+    if ctM <= 20:
+       cRM = 19 - ctM
+    elif ctM > 20:
+        cRM = 79 - ctM
+countRemMin()
+
+def countRemHr():
+    global cRH
+    countHr()
+    if ctH < 15:
+        cRH = 15 - ctH
+    elif ctH == 15 and ctM < 20:
+        cRH = 15 - ctH
+    elif ctH == 15 and ctM >= 20:
+        cRH = 24 - ctH + 15
+countRemHr()
+
+def countRemDay():
+    global cRD
+    cRD = diff
+    if cRH == 0 and cRM == 0:
+        cRD = diff - 1
+countRemDay()
+        
 
 def countDayTime():
-    H = int(time.strftime("%H", t))
-    M = int(time.strftime("%M", t))
-    S = int(time.strftime("%S", t))
-
-    if H == 0:
-        if M == 0 and S < 5:
-            getDate()
+   init()
+   if ctH == 0:
+        if ctM == 0 and ctS < 5:
+            #getDate()
             RD = diff
-            RH = 15 - H
-            RM = 19 - M
-            RS = 59 - S
-        if M < 20:
+            RH = 15 - ctH
+            RM = 19 - ctM
+            RS = 59 - ctS
+        if ctM < 20:
             RD = diff
-            RH = 15 - H
-            RM = 19 - M
-            RS = 59 - S
-        elif M == 20:
+            RH = 15 - ctH
+            RM = 19 - ctM
+            RS = 59 - ctS
+        elif ctM == 20:
             RD = diff
-            RH = 14 - H
+            RH = 14 - ctH
             RM = 59
-            RS = 59 - S
-        elif M > 20:
+            RS = 59 - ctS
+        elif ctM > 20:
             RD = diff
-            RH = 14 - H
-            RM = 79 - M
-            RS = 59 - S
-    elif H < 15:
+            RH = 14 - ctH
+            RM = 79 - ctM
+            RS = 59 - ctS
+   elif ctH < 15:
         RD = diff
-        if M < 20:
-            RH = 15 - H
-            RM = 19 - M
-            RS = 59 - S
-        elif M == 20:
-            RH = 14 - H
+        if ctM < 20:
+            RH = 15 - ctH
+            RM = 19 - ctM
+            RS = 59 - ctS
+        elif ctM == 20:
+            RH = 14 - ctH
             RM = 59
-            RS = 59 - S
-        elif M > 20:
-            RH = 14 - H
-            RM = 79 - M
-            RS = 59 - S
-    elif H == 15:
-        if M < 20:
+            RS = 59 - ctS
+        elif ctM > 20:
+            RH = 14 - ctH
+            RM = 79 - ctM
+            RS = 59 - ctS
+   elif ctH == 15:
+        if ctM < 20:
             RD = diff
-            RH = 15 - H
-            RM = 19 - M
-            RS = 59 - S
-        elif M == 20:
+            RH = 15 - ctH
+            RM = 19 - ctM
+            RS = 59 - ctS
+        elif ctM == 20:
             RD = diff - 1
             RH = 23
             RM = 59
-            RS = 59 - S
-        elif M > 20:
-            RH = 14 - H
-            RM = 79 - M
-            RS = 59 - S
-    elif H > 15:
-        if M < 20:
+            RS = 59 - ctS
+        elif ctM > 20:
+            RH = 14 - ctH
+            RM = 79 - ctM
+            RS = 59 - ctS
+   elif ctH > 15:
+        if ctM < 20:
             RD = diff - 1
-            RH = 24 - H + 15
-            RM = 19 - M
-            RS = 59 - S
-        elif M == 20:
+            RH = 24 - ctH + 15
+            RM = 19 - ctM
+            RS = 59 - ctS
+        elif ctM == 20:
             RD = diff - 1
-            RH = 23 - H + 15
+            RH = 23 - ctH + 15
             RM = 0
-            RS = 59 - S
-        elif M > 20:
+            RS = 59 - ctS
+        elif ctM > 20:
             RD = diff - 1
-            RH = 23 - H + 15
-            RM = 79 - M
-            RS = 59 - S
+            RH = 23 - ctH + 15
+            RM = 79 - ctM
+            RS = 59 - ctS
         
 
     
-    print ("Days Remaining:",RD, "Time Remaining:",RH,":",RM,":",RS)
-
- 
-
-    if H == 0 and M == 0:
-        getDate()
+   print ("Days Remaining:",RD, "Time Remaining:",RH,":",RM,":",RS)
 
 while True:
-    countDayTime()
-    countSec()
-    print (ctH,":",ctM,":",ctS)
+    #countDayTime()
+    countRemSec()
+    #countSec()
+    print ("Days Remaining:",cRD,"Time Remaining:",cRH,":",cRM,":",cRS)
     time.sleep(1)
