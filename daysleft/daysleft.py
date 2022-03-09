3219b3d28f6aad239ae055bf0a77921efa4556ab
Alex West <alexreidwest@gmail.com>
2022-03-09 10:42:19 -0600

    Fixed the thing where it wasn't counting stuff

--- daysleft/daysleft.py
+++ daysleft/daysleft.py
@@ -5,11 +5,6 @@ import time
 #initialize variables
 end = datetime.date(2022,5,27)
 daysOff = ['2022-03-21', '2022-03-22', '2022-03-23', '2022-03-24', '2022-03-25', '2022-04-15']
-#t = time.localtime()
-#ctS = int(time.strftime("%S", t))
-#ctM = int(time.strftime("%M", t))
-#ctH = int(time.strftime("%H", t))
-#print ("int",ctH,":",ctM,":",ctS)
 
 #define functions
 def getDate():
@@ -34,6 +29,7 @@ countSec()
 
 def countMin():
     global ctM
+    getTime()
     ctM = int(time.strftime("%M", t))
     if ctM == 0 and 0 <= ctS < 2:
         countHr()
@@ -41,6 +37,7 @@ countMin()
 
 def countHr():
     global ctH
+    getTime()
     ctH = int(time.strftime("%H", t))
     if ctH == 0 and ctS < 2:
         getDate()
@@ -56,29 +53,38 @@ def init():
 def countRemSec():
     global cRS
     countSec()
-    cRS = 59 - ctS
-    if cRS == 0:
+    cRS = 59  - ctS
+    if ctS == 0:
         countRemMin()
 countRemSec()
 
 def countRemMin():
     global cRM
     countMin()
-    if ctM <= 20:
+    if ctM < 20:
        cRM = 19 - ctM
-    elif ctM > 20:
+    elif ctM >= 20:
         cRM = 79 - ctM
+        #countRemHr()
 countRemMin()
 
 def countRemHr():
     global cRH
     countHr()
-    if ctH < 15:
-        cRH = 15 - ctH
-    elif ctH == 15 and ctM < 20:
-        cRH = 15 - ctH
-    elif ctH == 15 and ctM >= 20:
-        cRH = 24 - ctH + 15
+    if ctM < 20:
+        if ctH < 15:
+           cRH = 15 - ctH
+        elif ctH == 15:
+           cRH = 15 - ctH
+        elif ctH > 15:
+            cRH = 23 - ctH
+    elif ctM >= 20:
+        if ctH < 15:
+           cRH = 14 - ctH
+        elif ctH == 15:
+           cRH = 23
+        elif ctH > 15:
+            cRH = 23 - ctH
 countRemHr()
 
 def countRemDay():
@@ -87,81 +93,6 @@ def countRemDay():
     if cRH == 0 and cRM == 0:
         cRD = diff - 1
 countRemDay()
-        
-
-def countDayTime():
-   init()
-   if ctH == 0:
-        if ctM == 0 and ctS < 5:
-            #getDate()
-            RD = diff
-            RH = 15 - ctH
-            RM = 19 - ctM
-            RS = 59 - ctS
-        if ctM < 20:
-            RD = diff
-            RH = 15 - ctH
-            RM = 19 - ctM
-            RS = 59 - ctS
-        elif ctM == 20:
-            RD = diff
-            RH = 14 - ctH
-            RM = 59
-            RS = 59 - ctS
-        elif ctM > 20:
-            RD = diff
-            RH = 14 - ctH
-            RM = 79 - ctM
-            RS = 59 - ctS
-   elif ctH < 15:
-        RD = diff
-        if ctM < 20:
-            RH = 15 - ctH
-            RM = 19 - ctM
-            RS = 59 - ctS
-        elif ctM == 20:
-            RH = 14 - ctH
-            RM = 59
-            RS = 59 - ctS
-        elif ctM > 20:
-            RH = 14 - ctH
-            RM = 79 - ctM
-            RS = 59 - ctS
-   elif ctH == 15:
-        if ctM < 20:
-            RD = diff
-            RH = 15 - ctH
-            RM = 19 - ctM
-            RS = 59 - ctS
-        elif ctM == 20:
-            RD = diff - 1
-            RH = 23
-            RM = 59
-            RS = 59 - ctS
-        elif ctM > 20:
-            RH = 14 - ctH
-            RM = 79 - ctM
-            RS = 59 - ctS
-   elif ctH > 15:
-        if ctM < 20:
-            RD = diff - 1
-            RH = 24 - ctH + 15
-            RM = 19 - ctM
-            RS = 59 - ctS
-        elif ctM == 20:
-            RD = diff - 1
-            RH = 23 - ctH + 15
-            RM = 0
-            RS = 59 - ctS
-        elif ctM > 20:
-            RD = diff - 1
-            RH = 23 - ctH + 15
-            RM = 79 - ctM
-            RS = 59 - ctS
-        
-
-    
-   print ("Days Remaining:",RD, "Time Remaining:",RH,":",RM,":",RS)
 
 while True:
     #countDayTime()
